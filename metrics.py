0eed1e611627385063730c723c047374f2459015
Lukas Sandman <sandmanlukas@gmail.com>
2023-10-18 11:18:16 +0200

    Update metrics

--- metrics.py
+++ metrics.py
@@ -21,8 +21,9 @@ def ssim(image_pred, image_gt, reduction='mean'):
     """
     image_pred and image_gt: (1, 3, H, W)
     """
+    image_gt = image_gt if torch.is_tensor(image_gt) else torch.from_numpy(image_gt)
     image_pred = image_pred.permute(2,1,0).unsqueeze(0)
-    image_gt = torch.from_numpy(image_gt).permute(2,1,0).unsqueeze(0)
+    image_gt = image_gt.permute(2,1,0).unsqueeze(0)
     dssim_ = dssim(image_pred, image_gt, 3, reduction) # dissimilarity in [0, 1]
     return 1-2*dssim_ # in [-1, 1]
 
@@ -36,6 +37,7 @@ def normalize_negative_one(array):
 
 def lpips_score(image_pred, image_gt):
     # LPIPS require images in -1 to 1 range.
+    image_gt = image_gt.cpu().numpy() if torch.is_tensor(image_gt) else image_gt
     rgb_lpips = lpips.im2tensor(normalize_negative_one(image_pred.cpu().numpy()))
     gt_lpips = lpips.im2tensor(normalize_negative_one(image_gt))
 
